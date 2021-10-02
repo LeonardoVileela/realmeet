da2b5ea1fe4270abd9e7c591c34736b0432d8c1a
LeonardoVileela <leovilela.empresa@gmail.com>
2021-10-01 21:53:32 -0300

    first

---
+++ service/src/main/java/br/com/sw2you/realmeet/RoomController.java
@@ -0,0 +1,17 @@
+package br.com.sw2you.realmeet;
+
+import br.com.sw2you.realmeet.api.facade.RoomsApi;
+import br.com.sw2you.realmeet.api.model.Room;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+public class RoomController implements RoomsApi {
+
+    @Override
+    public CompletableFuture<ResponseEntity<Room>> listRooms(Long id) {
+        return CompletableFuture.supplyAsync(() -> ResponseEntity.ok(new Room().id(1L).name("Room 1")));
+    }
+}
